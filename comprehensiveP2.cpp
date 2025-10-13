4fac31b1cce84f4a6a6796c0c72d15c9267a90f2
Hyun-nine-CH <ttmstym234@gmail.com>
2025-10-13 18:21:51 +0900

    5

--- comprehensiveP2.cpp
+++ comprehensiveP2.cpp
@@ -4,10 +4,11 @@ using namespace std;
 void rectangle(int l, int r, int t, int b){
 
 }
+/*
 void rectangle(int w, int h, int x=0, int y=0){ // 디폴트 매개변수는 뒷쪽에 위치
 
 }
-
+*/
 int main(){
     rectangle(0,0,0,0);
 } // 매개변수의 개수가 4개로 같아 오버로딩 시, 문제가 될 수 있음
